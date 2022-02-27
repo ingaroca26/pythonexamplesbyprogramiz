dc6a93a5eb623ca32f1f352c955512d4b0756268
ingaroca26 <ingaroca26@outlook.com>
2022-02-27 01:42:28 -0500

    v0.0.5

---
+++ 02/sum.py
@@ -0,0 +1,5 @@
+first_summand: int = int(input('Enter first summand: '))
+second_summand: int = int(input('Enter second summand: '))
+sum: int = first_summand + second_summand
+f_sum: str = f'The sum of {first_summand} and {second_summand} is: {sum}.'
+print(f_sum)
